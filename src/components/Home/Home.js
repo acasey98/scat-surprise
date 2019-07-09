4fdeb5513e19b2229fcce265ddf991c5207d1124
Austin Casey <thepoast@gmail.com>
2019-07-08 20:00:33 -0500

    read

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import scatData from '../../helpers/data/scatData';
+
+import ScatCard from '../ScatCard/ScatCard';
+
 class Home extends React.Component {
+  state = {
+    scats: [],
+  }
+
+  componentDidMount() {
+    const { uid } = firebase.auth().currentUser;
+    scatData.getMyScats(uid)
+      .then((scats) => {
+        this.setState({ scats });
+      })
+      .catch(err => err);
+  }
+
   editEvent = (e) => {
     e.preventDefault();
     const orderId = '12345';
@@ -9,10 +28,19 @@ class Home extends React.Component {
   }
 
   render() {
+    const makeScatCards = this.state.scats.map(scat => (
+      <ScatCard
+        key={scat.id}
+        scat={scat}
+      />
+    ));
     const singleLink = '/scat/12345';
     return (
-      <div className="Home">
+      <div className="Home col">
         <div>Home</div>
+        <div className="d-flex">
+          {makeScatCards}
+        </div>
         <button className="btn btn-danger" onClick={this.editEvent}>Edit</button>
         <Link to={singleLink}>View Single</Link>
       </div>
